61474afd065e6b1acbccc1e4ea27446aa1214b39
gustavo <gustavoferri13@gmail.com>
2020-11-11 19:34:37 -0400

    ROUTES: add categorys routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,15 +2,24 @@ import { Router } from "express";
 import multer from "multer";
 
 import uploadConfig from "./config/upload";
+import CategoryController from "./controllers/CategoryController";
 import ProductsController from "./controllers/ProductsController";
 
 const routes = Router();
 const upload = multer(uploadConfig);
 
+// Products API
 routes.get("/products", ProductsController.index);
 routes.get("/products/:id", ProductsController.show);
 routes.post("/products", upload.array("photos"), ProductsController.create);
 routes.post("/products/:id", upload.array("photos"), ProductsController.update);
 routes.delete("/products/:id", ProductsController.delete);
+
+// Categorys API
+routes.get("/categorys", CategoryController.index);
+routes.get("/categorys/:id", CategoryController.show);
+routes.post("/categorys", CategoryController.create);
+routes.post("/categorys/:id", CategoryController.update);
+routes.delete("/categorys/:id", CategoryController.delete);
 
 export default routes;
