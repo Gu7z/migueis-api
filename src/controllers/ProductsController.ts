fd941e03ad4d31b871f101759d538c4d4259ec98
gustavo <gustavoferri13@gmail.com>
2020-11-11 19:33:48 -0400

    CONTROLLERS: fix image on update and add new relation with category

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -4,12 +4,13 @@ import Products from "../models/products";
 import productView from "../views/productsView";
 import * as Yup from "yup";
 import Images from "../models/image";
+import Category from "../models/category";
 
 export default {
   async index(_req: Request, res: Response) {
     const productsRepository = getRepository(Products);
     const products = await productsRepository.find({
-      relations: ["images"],
+      relations: ["images", "category"],
     });
 
     return res.json(productView.renderMany(products));
@@ -20,14 +21,20 @@ export default {
 
     const productsRepository = getRepository(Products);
     const product = await productsRepository.findOneOrFail(id, {
-      relations: ["images"],
+      relations: ["images", "category"],
     });
 
     return res.json(productView.render(product));
   },
 
   async create(req: Request, res: Response) {
-    const { name, description, price, quantity } = req.body;
+    const {
+      name,
+      description,
+      price,
+      quantity,
+      category: category_id,
+    } = req.body;
 
     const productsRepository = getRepository(Products);
 
@@ -36,11 +43,15 @@ export default {
       path: image.filename,
     }));
 
+    const categoryRepository = getRepository(Category);
+    const category = await categoryRepository.findOneOrFail(category_id);
+
     const data = {
       name,
       description,
       price,
       quantity,
+      category,
       images,
     };
 
@@ -54,6 +65,12 @@ export default {
           path: Yup.string().required(),
         })
       ),
+      category: Yup.object()
+        .shape({
+          name: Yup.string().required(),
+          id: Yup.number().required(),
+        })
+        .required(),
     });
 
     await schema.validate(data, {
@@ -62,7 +79,7 @@ export default {
 
     const product = productsRepository.create(data);
 
-    await productsRepository.save(product);
+    const teste = await productsRepository.save(product);
 
     return res.status(201).json(productView.render(product));
   },
@@ -70,12 +87,21 @@ export default {
   async update(req: Request, res: Response) {
     const { id } = req.params;
 
-    const { name, description, price, quantity } = req.body;
+    const {
+      name,
+      description,
+      price,
+      quantity,
+      category: category_id,
+    } = req.body;
 
     const productsRepository = getRepository(Products);
     const product = await productsRepository.findOneOrFail(id, {
-      relations: ["images"],
+      relations: ["images", "category"],
     });
+
+    const categoryRepository = getRepository(Category);
+    const category = await categoryRepository.findOneOrFail(category_id);
 
     const requestImages = req.files as Express.Multer.File[];
     const images = requestImages.map((image) => ({
@@ -87,6 +113,7 @@ export default {
       description,
       price,
       quantity,
+      category,
       images,
     };
 
@@ -100,6 +127,12 @@ export default {
           path: Yup.string().required(),
         })
       ),
+      category: Yup.object()
+        .shape({
+          name: Yup.string().required(),
+          id: Yup.number().required(),
+        })
+        .required(),
     });
 
     await schema.validate(data, {
@@ -113,7 +146,8 @@ export default {
     product.description = description;
     product.price = price;
     product.quantity = quantity;
-    product.images = [...product.images, ...imagesFromDB];
+    product.images = imagesFromDB;
+    product.category = category;
 
     await productsRepository.save(product);
 
